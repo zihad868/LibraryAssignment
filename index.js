655d0ad2abba4cbdea36c51aba61760d6b28fdbf
zihad868 <zihadh868@gmail.com>
2024-05-13 10:22:47 +0600

    Add Book API

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+const corsOptions = {
+  origin: ['http://localhost:5173/'],
+  credentials: true,
+  optionSuccessStatus: 200
+}
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 
 
 // var uri = "mongodb://<username>:<password>@ac-62j8ihz-shard-00-00.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-01.7lbrva6.mongodb.net:27017,ac-62j8ihz-shard-00-02.7lbrva6.mongodb.net:27017/?ssl=true&replicaSet=atlas-g1t94d-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
@@ -22,12 +37,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const libraryCollection = client.db('LibraryDB').collection('books');
+
+    // Get Book
+    
+
+    // Add Book
+    app.post('/addBook', async(req, res) => {
+      const book = req.body;
+      const result = await libraryCollection.insertOne(book);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
