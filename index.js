d17d9782756066edba5e3e89183963bc1e9f8d51
zihad868 <zihadh868@gmail.com>
2024-05-13 14:23:41 +0600

    Delete Book API

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
        res.send(result);
     })
 
+    //Delete Book
+    app.delete('/deleteBook/:id', async(req, res) => {
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+       const result = await libraryCollection.deleteOne(query);
+       res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
